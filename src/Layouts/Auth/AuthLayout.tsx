a091c0f0d094c7005fa05117418a56600ea0f56d
mahmoudhamed169 <mh4190207@gmail.com>
2024-09-13 21:41:14 +0300

    imporove responsive desgin

--- src/Layouts/Auth/AuthLayout.tsx
+++ src/Layouts/Auth/AuthLayout.tsx
@@ -1,26 +1,26 @@
 import { Outlet } from "react-router-dom";
 import AuthPageTitle from "../../Utils/AuthPageTitle";
 import MainTitle from "../../Components/SharedComponents/MainTitle/MainTitle";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Grid2 } from "@mui/material";
 import AuthBackGround from "../../Components/SharedComponents/AuthBackGround/AuthBackGround";
 
 export default function AuthLayout() {
   AuthPageTitle();
   return (
     <>
-      <Grid container spacing={2} sx={{ height: { xs: "auto", md: "100vh" } }}>
-        <Grid item md={6} sm={12}>
+      <Grid2 container spacing={3} sx={{ height: { xs: "auto", md: "100vh" } }}>
+        <Grid2 item size={{ md: 6, sm: 10, xs: 12 }}>
           <Box className="mt-5 ms-8">
             <MainTitle />
           </Box>
-          <Box className="mt-20 ms-28">
+          <Box className="mt-20 w-4/5 mx-auto md:ps-6">
             <Outlet />
           </Box>
-        </Grid>
-        <Grid item md={6} sm={12}>
+        </Grid2>
+        <Grid2 item size={{ md: 6, sm: 10, xs: 12 }}>
           <AuthBackGround />
-        </Grid>
-      </Grid>
+        </Grid2>
+      </Grid2>
     </>
   );
 }
