4715711472b7f8aaccedda8b0a0ac601f056b140
osama elgendy <osama elgendy.com>
2024-09-20 23:58:57 +0300

    stop on testing

--- src/Pages/AdminPages/Rooms/RoomsData/RoomsData.tsx
+++ src/Pages/AdminPages/Rooms/RoomsData/RoomsData.tsx
@@ -1,5 +1,5 @@
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 import {
   Button,
   Checkbox,
@@ -15,23 +15,19 @@ import {
 } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import { useState, useEffect, useCallback } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { set, SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-// import Styles from "./AddNewRoom.module.scss";
 import { InputLabel, MenuItem } from "@mui/material";
 import axios from "axios";
-// ! in api file create propety BASE_HEADER
 import { ADMIN_ROOM } from "../../../../Api/END_POINTS";
-// import { IAddRoom } from "../../../../interface/RoomInterface";
-// ! import dreab and drap
-// import DragDropFileUpload from "../../../../shared/DragDropFileUpload/DragDropFileUpload";
-// import useFacilities from "../../../Hook/useFacilities";
 import { toast } from "react-toastify";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import CloudUpload from "@mui/icons-material/CloudUpload";
+import { i } from "framer-motion/client";
 const RoomsData = () => {
   const navigate = useNavigate();
-
+  let [disablefacilites, setdisablefacilites] = useState(0);
+  let [facilitesCheck, setfacilitesCheck] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
   const [facilitiesList, setfacilitiesList] = useState([]);
@@ -49,7 +45,6 @@ const RoomsData = () => {
     formState: { errors },
   } = useForm<IAddRoom>();
 
-  // Format Data
   const appendToFormData = (data: IAddRoom) => {
     const formData = new FormData();
     formData.append("roomNumber", data?.roomNumber);
@@ -63,7 +58,6 @@ const RoomsData = () => {
       });
     }
 
-    // Append multiple images
     for (let i = 0; i < images.length; i++) {
       formData.append("imgs", images[i]);
     }
@@ -81,16 +75,7 @@ const RoomsData = () => {
 
   const onSubmit: SubmitHandler<IAddRoom> = async (data: IAddRoom) => {
     setIsLoading(true);
-    //! ask mohamed aill  monter UPskilling
-    // Check if discount is outside the range 0 to 100
-    // if (
-    //   data.discount !== undefined &&
-    //   (data.discount < 0 || data.discount > 100)
-    // ) {
-    //   toast.error("Discount must be between 0 and 100.");
-    //   return;
-    // }
-    const addFormData = appendToFormData(data);
+    addFormData = appendToFormData(data);
     setIsLoading(true);
     axios
       .post(ADMIN_ROOM.createRoom, addFormData, {
@@ -106,28 +91,6 @@ const RoomsData = () => {
       .finally(() => setIsLoading(false));
   };
 
-  // const handleFileUpload = (file :any) => {
-  //   setSelectedImage(URL.createObjectURL(files[0]));
-  // };
-
-  // const handleFileUpload = useCallback(
-  //   (files) => {
-  //     const imageUrls = Array.from(files).map((file) => URL.createObjectURL(file));
-  //     console.log(imageUrls);
-  //     setSelectedImage(imageUrls);
-  //   },
-  //   []
-  // );
-
-  // const handleFileUpload = useCallback(
-  //   (files) => {
-  //     const imageUrls = Array.from(files).map((file) => URL.createObjectURL(file));
-  //     console.log(imageUrls);
-  //     setSelectedImage(imageUrls);
-  //   },
-  //   []
-  // );
-
   const getAllFacilities = () => {
     setIsLoading(true);
 
@@ -137,7 +100,7 @@ const RoomsData = () => {
       })
       .then((response) => {
         setfacilitiesList(response?.data?.data?.facilities);
-        
+
         setIsLoading(false);
       })
       .catch(() => {
@@ -146,31 +109,17 @@ const RoomsData = () => {
       });
   };
 
-  const waitDatainFacility = async() =>{
-    await  getAllFacilities();
- }
-  useEffect( () => {
-  
-
-    waitDatainFacility()
+  const waitDatainFacility = async () => {
+    await getAllFacilities();
+  };
+  useEffect(() => {
+    waitDatainFacility();
     console.log(1);
   }, []);
 
-  // const handleChangePage = (event, newPage) => {
-  //     setCurrentPage(newPage + 1);
-  // };
-
-  // const handleChangeRowsPerPage = (event) => {
-  //     setRowsPerPage(parseInt(event.target.value, 10));
-  //     setCurrentPage(1); // Reset to the first page when changing rows per page
-  // };
   return (
     <>
-      <Container
-        component="main"
-        // className={``}
-        sx={{ width: "100%", padding: 0 }}
-      >
+      <Container component="main" sx={{ width: "100%", padding: 0 }}>
         <Grid container justifyContent="center">
           <Grid
             item
@@ -197,7 +146,6 @@ const RoomsData = () => {
                 component="form"
                 noValidate
                 onSubmit={handleSubmit(onSubmit)}
-                // encType="multipart/form-data"
                 sx={{
                   width: "100%",
                   maxWidth: "none",
@@ -218,7 +166,15 @@ const RoomsData = () => {
                   }}
                 />
                 {errors.roomNumber && errors.roomNumber.type === "required" && (
-                  <span style={{color:"red ", fontWeight:"600",textTransform:"capitalize"}}>This field is required</span>
+                  <span
+                    style={{
+                      color: "red ",
+                      fontWeight: "600",
+                      textTransform: "capitalize",
+                    }}
+                  >
+                    This field is required
+                  </span>
                 )}
 
                 <Grid container spacing={2}>
@@ -239,7 +195,15 @@ const RoomsData = () => {
                       }}
                     />
                     {errors.price && errors.price.type === "required" && (
-                      <span  style={{color:"red ", fontWeight:"600",textTransform:"capitalize"}}  >This field is required</span>
+                      <span
+                        style={{
+                          color: "red ",
+                          fontWeight: "600",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        This field is required
+                      </span>
                     )}
                   </Grid>
 
@@ -249,7 +213,7 @@ const RoomsData = () => {
                       required
                       id="filled-required"
                       label="Capacity"
-                      // variant="filled"
+                      variant="filled"
                       fullWidth
                       sx={{
                         width: "100%",
@@ -257,7 +221,15 @@ const RoomsData = () => {
                       }}
                     />
                     {errors.capacity && errors.capacity.type === "required" && (
-                      <span style={{color:"red ", fontWeight:"600",textTransform:"capitalize"}}>This field is required</span>
+                      <span
+                        style={{
+                          color: "red ",
+                          fontWeight: "600",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        This field is required
+                      </span>
                     )}
                   </Grid>
                 </Grid>
@@ -268,7 +240,6 @@ const RoomsData = () => {
                       {...register("discount", {
                         valueAsNumber: true,
                         required: true,
-                        // pattern: /^(?!0$|100$)\d+$/,
                       })}
                       required
                       id="discount"
@@ -284,10 +255,26 @@ const RoomsData = () => {
                       }}
                     />
                     {errors.discount && errors.discount.type === "required" && (
-                      <span style={{color:"red ", fontWeight:"600",textTransform:"capitalize"}}>This field is required</span>
+                      <span
+                        style={{
+                          color: "red ",
+                          fontWeight: "600",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        This field is required
+                      </span>
                     )}
                     {errors.discount && errors.discount.type === "pattern" && (
-                      <span style={{color:"red ", fontWeight:"600",textTransform:"capitalize"}}>Invalid discount value</span>
+                      <span
+                        style={{
+                          color: "red ",
+                          fontWeight: "600",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        Invalid discount value
+                      </span>
                     )}
                   </Grid>
 
@@ -308,10 +295,11 @@ const RoomsData = () => {
                             shouldValidate: true,
                           })
                         }
+                        onClick={() => {}}
                         sx={{ width: "100%" }}
                         renderValue={(selected) => (
                           <div>
-                            {selected?.map((value) => (
+                            {selected.map((value) => (
                               <span key={value} style={{ marginRight: "8px" }}>
                                 {facilitiesList.find(
                                   (facility) => facility._id === value
@@ -321,49 +309,71 @@ const RoomsData = () => {
                           </div>
                         )}
                       >
-                        {facilitiesList?.map((facility) => (
-                          <MenuItem key={facility?._id} value={facility?._id}>
+                        {facilitiesList.map((facility) => (
+                          <MenuItem
+                            key={facility._id}
+                            disabled={facilitesCheck}
+                            value={facility._id}
+                          >
                             <Checkbox
+                              onClick={(e) => {
+                                if (e.target.checked) {
+                                  setdisablefacilites((disablefacilites += 1));
+                                  if (disablefacilites > 4) {
+                                    setfacilitesCheck(true);
+                                  }
+                                } else {
+                                  setdisablefacilites((disablefacilites -= 1));
+                                }
+                              }}
                               checked={watch("facilities")?.includes(
-                                facility?._id
+                                facility._id
                               )}
                             />
-                            <ListItemText primary={facility?.name} />
+                            <ListItemText primary={facility.name} />
                           </MenuItem>
                         ))}
                       </Select>
                     </FormControl>
                     {errors.facilities &&
                       errors.facilities.type === "required" && (
-                        <span style={{color:"red ", fontWeight:"600",textTransform:"capitalize"}}>
+                        <span
+                          style={{
+                            color: "red ",
+                            fontWeight: "600",
+                            textTransform: "capitalize",
+                          }}
+                        >
                           Facilities are required
                         </span>
                       )}
+                    {disablefacilites > 4 && (
+                      <Button
+                      
+                        variant="contained"
+                        onClick={async () => {
+                          const deletItemFacilites: Number =
+                            watch("facilities").length;
+                          watch("facilities").splice(0, deletItemFacilites);
+                          setdisablefacilites(0);
+                          setfacilitesCheck(false);
+                        }}
+                      >
+                        Clear All Select 
+                      </Button>
+                    )}
                   </Grid>
                 </Grid>
                 <div style={{ padding: 50 }}>
-                  {/* <DragDropFileUpload  /> */}
-                  <Paper
-                    variant="outlined"
-                    // onDragOver={handleDragOver}
-                    // onDragLeave={handleDragLeave}
-                    // onDrop={handleDrop}
-                    // style={{
-                    //   border: dragOver ? "2px dashed #000" : "2px dashed #aaa",
-                    //   padding: 20,
-                    //   textAlign: "center",
-                    //   cursor: "pointer",
-                    //   background: dragOver ? "#eee" : "#fafafa",
-                    // }}
-                  >
+                  <Paper variant="outlined">
                     <input
                       accept="image/*"
                       style={{ display: "none" }}
                       id="raised-button-file"
                       multiple
                       type="file"
-                      onChange={(e) =>{
-                        // convert file 
+                      onChange={(e) => {
+                        // convert file
                         console.log(e.target);
                         const files = Array.from(e.target.files);
                         setImages(files);
@@ -390,12 +400,17 @@ const RoomsData = () => {
                     </label>
                   </Paper>
                   {selectedImage && (
-                    <div style={{ marginTop: '20px' }}>
+                    <div style={{ marginTop: "20px" }}>
                       {selectedImage.map((imageUrl, index) => (
-                        <img key={index} src={imageUrl} alt={`Selected ${index + 1}`} style={{ maxWidth: '80%', maxHeight: '100px' }} />
+                        <img
+                          key={index}
+                          src={imageUrl}
+                          alt={`Selected ${index + 1}`}
+                          style={{ maxWidth: "80%", maxHeight: "100px" }}
+                        />
                       ))}
                     </div>
-                  )} 
+                  )}
                 </div>
                 <Box>
                   <label htmlFor="upload-input">
@@ -415,16 +430,25 @@ const RoomsData = () => {
                     multiple
                     style={{ display: "none" }}
                   />
-                    {images.length > 5 &&
-                       <Stack sx={{ width: '100%', margin:"10px" }} spacing={2}>
-                       <Alert severity="error">please enter 5 images or enter   afew form five imges  .</Alert>
-                     </Stack>
-                       } 
+                  {images.length > 5 && (
+                    <Stack sx={{ width: "100%", margin: "10px" }} spacing={2}>
+                      <Alert severity="error">
+                        please enter 5 images or enter afew form five imges .
+                      </Alert>
+                    </Stack>
+                  )}
                   {images.length > 0 && (
-                    <div style={{ marginTop: "20px"   , display:"flex" , justifyContent:"center" ,  gap:"10px",flexWrap:"Wrap", alignItems:"center"}}>
-                  
+                    <div
+                      style={{
+                        marginTop: "20px",
+                        display: "flex",
+                        justifyContent: "center",
+                        gap: "10px",
+                        flexWrap: "Wrap",
+                        alignItems: "center",
+                      }}
+                    >
                       {images.map((file, index) => (
-                        
                         <img
                           key={index}
                           src={URL.createObjectURL(file)}
@@ -434,7 +458,6 @@ const RoomsData = () => {
                             maxHeight: "100px",
                             margin: "5px",
                           }}
-                          
                         />
                       ))}
                     </div>
