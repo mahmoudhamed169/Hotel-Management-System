8a663601df3c301c7c65f5ab535fb8856798f24a
osama elgendy <osama elgendy.com>
2024-09-23 16:01:22 +0300

    NavBar&&Footer

--- src/Pages/UserPages/Landing/Landing.tsx
+++ src/Pages/UserPages/Landing/Landing.tsx
@@ -1,3 +1,12 @@
+import Footer from "../navbar&&footer/Footer";
+import NavBarFooter from "../navbar&&footer/NavBarFooter";
+
 export default function Landing() {
-  return <div></div>;
+  return <div>
+    <NavBarFooter/>
+    <br/>
+    <br/>
+    <br/>
+    <Footer/>
+  </div>;
 }
