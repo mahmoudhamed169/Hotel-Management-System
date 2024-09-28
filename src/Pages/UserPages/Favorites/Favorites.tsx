56d0b922c8aeef8d7acb9a658a791b02dfd3e439
Abdelrahman Mabrouk <Amabrouk@jeraisy.com>
2024-09-28 21:03:57 +0300

    favourite list

--- src/Pages/UserPages/Favorites/Favorites.tsx
+++ src/Pages/UserPages/Favorites/Favorites.tsx
@@ -1,45 +1,53 @@
-import { Box, Grid2 as Grid, Typography } from "@mui/material";
+import { Box, Grid2 as Grid, Skeleton, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { apiClient, PORTAL_URLS } from "../../../Api/END_POINTS";
 import { PhotoCard } from "../../../Components/AdminSharedComponents/PhotoCard/PhotoCard";
+import toast from "react-hot-toast";
+import { AxiosError } from "axios";
+
+interface RoomType {
+  _id: string;
+  roomNumber: string;
+  images: string[];
+  price: number;
+}
+
+interface FavRoom {
+  favoriteRooms:[
+    rooms
+  ]
+}
+
+interface fav {
+  rooms:RoomType
+}
+
+interface IResponse {
+    data:FavRoom
+}
 
 
-
-// interface RoomType {
-//   _id: string;
-//   roomNumber: string;
-//   images: string[];
-//   price: number;
-// }
-
-// interface FavType {
-//   rooms: RoomType;
-// }
-
-// interface FavResponse {
-//   data: {
-//     favoriteRooms: FavoriType[];
-//   };
-// }
 
 
 
 export default function Favorites() {
-
-const [favList , setFavList]=useState([])
+  const [loading, setLoading] =useState<boolean>(true);
+const [favList , setFavList]=useState<RoomType[]>([])
 console.log(favList);
 
 const getFavlist = async ()=>{
 
 
   try {
-    const response = await apiClient.get( PORTAL_URLS.favoriRoom)
+    const response = await apiClient.get<IResponse>( PORTAL_URLS.favoriRoom)
 
-    console.log(response.data.data.favoriteRooms[0].rooms);
+    console.log(response);
     setFavList(response.data.data.favoriteRooms[0].rooms)
+    
   } catch (error) {
 
-    console.log(error);
+    const axiosError = error as AxiosError;
+      toast.error(axiosError.message);
     
   }
 }
@@ -51,10 +59,13 @@ const removeFaveList = async (roomId: string)=>{
     const response = await apiClient.delete(`${PORTAL_URLS.favoriRoom}/${roomId}`,{
       data:{roomId}
     });
+
    getFavlist()
+   toast.success(response.data.data.message || 'favourite room removed' )
    
   } catch (error) {
-    console.log(error);
+    const axiosError = error as AxiosError;
+    toast.error(axiosError.message);
     
   }
 }
@@ -63,12 +74,20 @@ useEffect(()=>{
   getFavlist()
 },[])
 
+useEffect(() => {
+  const timer = setTimeout(() => {
+    setLoading(false);
+  }, 1700); 
 
-
+  return () => clearTimeout(timer);
+}, []);
 
   return (
     <>
-      <Box
+ 
+    
+      
+        <Box
         sx={{
           width: "85%",
           margin: "auto",
@@ -89,6 +108,7 @@ useEffect(()=>{
         >
           Your Favorites
         </Typography>
+   
         <Box>
           <Typography
             variant="h3"
@@ -101,19 +121,36 @@ useEffect(()=>{
             Your Rooms
           </Typography>
 
+{ loading?
+               
+                <Box sx={{paddingTop:'20px'}}>
+                <Grid container spacing={2}>
+                {favList?.map((fav)=>
+                    <Grid size={{ xs: 12, lg: 4 }}>
+                      <Skeleton variant="text" width="150px" animation="wave" height='100px'/>
+                    </Grid>)
+                    
+                    
+                    }
+                </Grid>
+              </Box>
+:
           <Box sx={{paddingTop:'20px'}}>
             <Grid container spacing={2}>
             {favList?.map((fav)=>
-                <Grid size={{ xs: 12, lg: 3 }}>
+                <Grid size={{ xs: 12, lg: 4 }}>
                   <PhotoCard
                   value={fav}                 
                   isFavorite={true}
                   eyeIcon={true}
                   onToggleFavorite={()=>removeFaveList(fav._id)}
                   />
-                </Grid>)}
+                </Grid>)
+                
+                
+                }
             </Grid>
-          </Box>
+          </Box>}
         </Box>
       </Box>
     </>
